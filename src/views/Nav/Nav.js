a0474f09f25e5f8d47f49c9a3e5128f037f2c7c1
gabrielgoncalfreitas <gabriel.goncal.freitas@gmail.com>
2023-02-03 20:22:23 -0300

    main

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,38 +1,29 @@
+import { useState } from "react";
+import { Routes } from "../../routes/Routes";
+import NavDropdownItem from "./components/NavDropdownItem";
+
 function Nav() {
+    const [dark, setDark] = useState(true);
+
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href>Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href={Routes.home.name}>CalCAD</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href>Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href>Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href>Action</a></li>
-                                    <li><a class="dropdown-item" href>Another action</a></li>
-                                    <li><hr class="dropdown-divider" /></li>
-                                    <li><a class="dropdown-item" href>Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href>Disabled</a>
-                            </li>
+                    <div className="collapse navbar-collapse" id="navbar">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <NavDropdownItem />
                         </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex" role="search">
+                            <div className="form-check form-switch">
+                                <input className="form-check-input" type="checkbox" role="switch" id="dark" checked={dark}
+                                    onChange={() => { dark ? setDark(false) : setDark(true) }} />
+                            </div>
                         </form>
                     </div>
                 </div>
